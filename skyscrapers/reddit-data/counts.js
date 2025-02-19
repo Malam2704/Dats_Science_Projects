b5eaf41dc0b2db21858d224efef942ce8f7211d0
Mohammed Alam <ma3655@g.rit.edu>
2025-02-18 23:36:00 -0500

    coutns made and visualziation setup

---
+++ skyscrapers/reddit-data/counts.js
@@ -0,0 +1,66 @@
+const cityCounts = {
+    "New York City": 23,
+    "Chicago": 8,
+    "Moscow": 4,
+    "Atlanta": 3,
+    "London": 3,
+    "Shanghai": 3,
+    "Philadelphia": 2,
+    "Panama City": 2,
+    "Seattle": 2,
+    "Shenzhen": 2,
+    "Frankfurt": 2,
+    "Guangzhou": 2,
+    "Mecca": 1,
+    "Saint Petersburg": 1,
+    "Baghdad": 1,
+    "Los Angeles": 1,
+    "Paris": 1,
+    "Jersey City": 1,
+    "Miami": 1,
+    "Manila": 1,
+    "Rome": 1,
+    "Albany": 1,
+    "West Hollywood": 1,
+    "Mumbai": 1,
+    "Limassol": 1,
+    "Nairobi": 1,
+    "Dubai": 1,
+    "Pyongyang": 1,
+    "Boston": 1,
+    "Austin": 1,
+    "Chongqing": 1,
+    "Monterrey": 1,
+    "Montreal": 1,
+    "Toronto": 1
+};
+
+const missingLines = [
+    1,  // "What city has the best skyline/mountain combo?" vancouver
+    3,  // "WOW!!!" nyc
+    10, // "Empire State Building" nyc
+    14, // "So slept on" detroit
+    17, // "The First-ever Skyscraper to Exist :" chicago
+    20, // "Why do these buildings have so much aura" nyc
+    27, // "How would the Freedom Tower look without the spire?" nyc
+    28, // "The Three Crowns of the Ohio River" pittsbrugh
+    29, // "Billionaires Row amongst the clouds" nyc
+    36, // "Million dollar bathtub view 🌇" nyc
+    38, // "What are skyscrapers called in your native language?" the hague
+    39, // "Greatest City in the World" london
+    46, // "lol" san jose
+    47, // "Skyscrapers across the Water" goldcoast
+    50, // "Icons in the sky" nyc
+    55, // "Some skyline records (the record holder for most 150 m, 200 m, 300 m, and 400 m buildings are all different!)" none
+    56, // "The most Multicultural city in the world" toronto
+    60, // "Do you prefer the John Hancock Center or Sears Tower?"chicago
+    69, // "North Korea skyline!" pyongyang
+    70, // "Some of the world’s most breathtaking streets/roads surrounded by skyscrapers - where would you like to drive?" shenzhen
+    83, // "Aeternitas Tower - 450m / 1, 476ft." dubai
+    90, // "206m / 677ft." boston
+    93, // "Built in era of art deco skyscrapers, One Grand Central Place (1930) is a stunning and underrated neo-Gothic skyscraper" nyc
+    102 // "2/3 of the posts here" none
+];
+
+console.log("City Counts:", cityCounts);
+console.log("Missing Lines:", missingLines);
